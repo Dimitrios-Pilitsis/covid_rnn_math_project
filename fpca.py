8d44271edd3f79c7d4d8a0838ed1717a26640196
Dimitrios Pilitsis <dimipili@gmail.com>
2021-05-15 15:58:19 +0300

    FPCA joint and vertical complete

--- fpca.py
+++ fpca.py
@@ -1,15 +1,9 @@
-"""
-import skfda
-from skfda.datasets import fetch_growth
-from skfda.exploratory.visualization import plot_fpca_perturbation_graphs
-from skfda.preprocessing.dim_reduction.projection import FPCA
-"""
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from math import isnan
+from fdasrsf import fPCA, time_warping, fdawarp, fdahpca
+
 
 countries=['Aruba', 'Afghanistan', 'Angola', 'Albania', 'Andorra', 'United_Arab_Emirates', 'Argentina', 'Australia', 'Austria', 'Azerbaijan', 'Burundi', 'Belgium', 'Benin', 'Burkina_Faso', 'Bangladesh', 'Bulgaria', 'Bahrain', 'Bahamas', 'Bosnia_and_Herzegovina', 'Belarus', 'Belize', 'Bermuda', 'Bolivia', 'Brazil', 'Barbados', 'Brunei', 'Bhutan', 'Botswana', 'Central_African_Republic', 'Canada', 'Switzerland', 'Chile', 'China', "Cote_d'Ivoire", 'Cameroon', 'Democratic_Republic_of_Congo', 'Congo', 'Colombia', 'Cape_Verde', 'Costa_Rica', 'Cuba', 'Cyprus', 'Czech_Republic', 'Germany', 'Djibouti', 'Dominica', 'Denmark', 'Dominican_Republic', 'Algeria', 'Ecuador', 'Egypt', 'Eritrea', 'Spain', 'Estonia', 'Ethiopia', 'Finland', 'Fiji', 'France', 'Faeroe_Islands', 'Gabon', 'United_Kingdom', 'Georgia', 'Ghana', 'Guinea', 'Gambia', 'Greece', 'Greenland', 'Guatemala', 'Guam', 'Guyana', 'Hong_Kong', 'Honduras', 'Croatia', 'Haiti', 'Hungary', 'Indonesia', 'India', 'Ireland', 'Iran', 'Iraq', 'Iceland', 'Israel', 'Italy', 'Jamaica', 'Jordan', 'Japan', 'Kazakhstan', 'Kenya', 'Kyrgyz_Republic', 'Cambodia', 'South_Korea', 'Kuwait', 'Laos', 'Lebanon', 'Liberia', 'Libya', 'Liechtenstein', 'Sri_Lanka', 'Lithuania', 'Luxembourg', 'Latvia', 'Macao', 'Morocco', 'Monaco', 'Moldova', 'Madagascar', 'Mexico', 'Mali', 'Malta', 'Myanmar', 'Mongolia', 'Mozambique', 'Mauritania', 'Mauritius', 'Malaysia', 'Namibia', 'Niger', 'Nigeria', 'Nicaragua', 'Netherlands', 'Norway', 'Nepal', 'New_Zealand', 'Oman', 'Pakistan', 'Panama', 'Peru', 'Philippines', 'Papua_New_Guinea', 'Poland', 'Puerto_Rico', 'Portugal', 'Paraguay', 'Palestine', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saudi_Arabia', 'Sudan', 'Senegal', 'Singapore', 'Sierra_Leone', 'El_Salvador', 'San_Marino', 'Somalia', 'Serbia', 'Suriname', 'Slovak_Republic', 'Slovenia', 'Sweden', 'Eswatini', 'Seychelles', 'Syria', 'Chad', 'Togo', 'Thailand', 'Trinidad_and_Tobago', 'Tunisia', 'Turkey', 'Taiwan', 'Tanzania', 'Uganda', 'Ukraine', 'Uruguay', 'United_States', 'Uzbekistan', 'Venezuela', 'United_States_Virgin_Islands', 'Vietnam', 'South_Africa', 'Zambia', 'Zimbabwe']
 
@@ -39,9 +33,7 @@ def setup_dataset(country_name):
 	return ts
 
 
-
-
-def main():
+def plot_all_curves():
 	fig, ax = plt.subplots(figsize=(10.0, 5.0))
 	time = np.arange(365)
 
@@ -50,6 +42,55 @@ def main():
 		ax.scatter(time, cases) 
 
 	#plt.savefig('./cases_figs/all_countries_cases')
+	plt.show()
+
+
+
+# Save all cases as a npy file for quicker handling
+def save_all_cases(countries):
+	cases_all_countries = []
+	for i, country in enumerate(countries):
+		cases = setup_dataset(country)	
+		cases_all_countries.append(cases)
+
+	np.save('fpca', cases_all_countries)
+
+
+
+
+
+
+def main():
+	time = np.arange(365).astype('double')
+	print(time.shape)
+	#save_all_cases(countries)
+	f = np.load('fpca.npy').astype('double').T #Need data in the form (days, countries)
+	print(f.shape)
+	
+	warp_f = time_warping.fdawarp(f, time)
+	warp_f.srsf_align()
+
+	#warp_f.plot()
+
+
+	#FPCA
+	#fPCA_analysis = fPCA.fdavpca(warp_f)
+	fPCA_analysis = fPCA.fdajpca(warp_f)
+
+
+	# Run the FPCA on a 3 components basis 
+	num_components = 10
+	fPCA_analysis.calc_fpca(no=num_components)
+	#fPCA_analysis.plot()
+
+	print(fPCA_analysis.f_pca.shape)
+	
+	fig, ax = plt.subplots(figsize=(10.0, 5.0))
+	for i in range(num_components):
+		ax.scatter(time, fPCA_analysis.f_pca[:,0,i])
+	
+	#plt.savefig('./cases_figs/fpca_vertical')
+	plt.savefig('./cases_figs/fpca_joint')
 	plt.show()
 
 
